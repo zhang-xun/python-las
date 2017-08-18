0ac5be2a0ce4aa5a5ef2e1cfe05bcbfcc6512e26
zhangxun <15025689012@163.com>
2017-08-18 17:30:44 +0800

    点云上下文类， 记录点云的全局参数

--- DataSet/PCDataSet/PCContext.py
+++ DataSet/PCDataSet/PCContext.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 # _*_ coding:utf-8  _*_
 __author__ = 'zhangxun'
+"""
+点云上下文类， 记录点云的全局参数
+"""
+from Sptial.SptialIndex import SptialIndex
 
 
-class PCContext:
+
+
+
+class PCContext(object):
     def __init__(self):
         self.GOffSetX = 0.0
         self.GOffsetY = 0.0
@@ -13,7 +20,7 @@ class PCContext:
         self.isBlockUseGlobalXYZ = True  # Block中使用全局XYZ坐标进行计算Key值，否则使用偏移后的值
 
         self.nSegmentSize = 1  # 分段大小，单位M
-        self._SptailIndex = None
+        self._SptailIndex = SptialIndex()
         self._Inst = None
 
     def instances(self):
